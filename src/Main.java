eb6e7d37ccbe0c4cacb7ae46966967b8fc4fdd22
Veronazavr <verochka10@list.ru>
2020-08-16 19:40:57 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        float x = 20;
+        float ticket = 5000;
+        float mile = ticket / x;
+
+        System.out.println(mile);
+
+
+    }
+}
